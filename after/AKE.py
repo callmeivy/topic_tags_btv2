82f7b04bf0df809b927b2be177f0085ee7fb5585
callmeivy <c.ivy.jin@gmail.com>
2017-11-29 15:42:40 +0800

    AKE
    AKE

---
+++ after/AKE.py
@@ -0,0 +1,192 @@
+#coding=UTF-8
+import jieba
+import jieba.posseg as pseg
+from itertools import takewhile, tee, izip
+import networkx, nltk
+import itertools, nltk, string
+dicFile = open('stopwords.txt', 'r')
+stopwords = dicFile.readlines()
+stopwordList = []
+stopwordList.append(' ')
+for stopword in stopwords:
+    temp = stopword.strip().replace('\r\n', '').decode('utf8')
+    stopwordList.append(temp)
+dicFile.close()
+path = "customize_dict.txt"
+jieba.load_userdict(path)
+import gensim, nltk
+from  textrank4zh import TextRank4Keyword,TextRank4Sentence
+import codecs
+valid = ['ns', 'vn','n','nz','eng','nr','nrt']
+#from http://bdewilde.github.io/blog/2014/09/23/intro-to-automatic-keyphrase-extraction/
+def extract_candidate_features(candidates, doc_text, doc_excerpt, doc_title):
+    import collections, math, nltk, re
+    
+    candidate_scores = collections.OrderedDict()
+    
+    # get word counts for document
+    doc_word_counts = collections.Counter(word.lower()
+                                          for sent in nltk.sent_tokenize(doc_text)
+                                          for word in nltk.word_tokenize(sent))
+    
+    for candidate in candidates:
+        
+        pattern = re.compile(r'\b'+re.escape(candidate)+r'(\b|[,;.!?]|\s)', re.IGNORECASE)
+        
+        # frequency-based
+        # number of times candidate appears in document
+        cand_doc_count = len(pattern.findall(doc_text))
+        # count could be 0 for multiple reasons; shit happens in a simplified example
+        if not cand_doc_count:
+            print '**WARNING:', candidate, 'not found!'
+            continue
+    
+        # statistical
+        candidate_words = candidate.split()
+        max_word_length = max(len(w) for w in candidate_words)
+        term_length = len(candidate_words)
+        # get frequencies for term and constituent words
+        sum_doc_word_counts = float(sum(doc_word_counts[w] for w in candidate_words))
+        try:
+            # lexical cohesion doesn't make sense for 1-word terms
+            if term_length == 1:
+                lexical_cohesion = 0.0
+            else:
+                lexical_cohesion = term_length * (1 + math.log(cand_doc_count, 10)) * cand_doc_count / sum_doc_word_counts
+        except (ValueError, ZeroDivisionError) as e:
+            lexical_cohesion = 0.0
+        
+        # positional
+        # found in title, key excerpt
+        in_title = 1 if pattern.search(doc_title) else 0
+        in_excerpt = 1 if pattern.search(doc_excerpt) else 0
+        # first/last position, difference between them (spread)
+        doc_text_length = float(len(doc_text))
+        first_match = pattern.search(doc_text)
+        abs_first_occurrence = first_match.start() / doc_text_length
+        if cand_doc_count == 1:
+            spread = 0.0
+            abs_last_occurrence = abs_first_occurrence
+        else:
+            for last_match in pattern.finditer(doc_text):
+                pass
+            abs_last_occurrence = last_match.start() / doc_text_length
+            spread = abs_last_occurrence - abs_first_occurrence
+
+        candidate_scores[candidate] = {'term_count': cand_doc_count,
+                                       'term_length': term_length, 'max_word_length': max_word_length,
+                                       'spread': spread, 'lexical_cohesion': lexical_cohesion,
+                                       'in_excerpt': in_excerpt, 'in_title': in_title,
+                                       'abs_first_occurrence': abs_first_occurrence,
+                                       'abs_last_occurrence': abs_last_occurrence}
+
+    return candidate_scores
+
+# 主要的
+def score_keyphrases_by_textrank(text, n_keywords=50):
+    word_nominal = dict()
+    text1 = pseg.cut(text)
+    for i in text1:
+        if i.word not in stopwordList:
+            # if len(i) == 0:
+            #     print 'kkk',i
+            if len(i.word) > 1:
+                # if (i != "test") and (i != "title") and (i != "content") and (i != "description") and (i != "time") and (len(i) != 8):
+                # if chara in valid:
+                word_nominal[i.word] = i.flag
+    keyphrases = dict()
+    word_2 = TextRank4Keyword()
+    word_2.analyze(text, window=2, lower=True)
+    w_list = word_2.get_keywords(num=30, word_min_len=1)
+    for w in w_list:
+        if w.word not in stopwordList:
+            if len(w.word)>1:
+                # 两种分词方法的交集
+                if w.word in word_nominal.keys():
+                    if word_nominal[w.word] in valid:
+                        # print 'ddddd',w.word,word_nominal[w.word]
+                        keyphrases[w.word] = w.weight
+    return sorted(keyphrases.iteritems(), key=lambda x: x[1], reverse=True)
+
+def score_keyphrases_by_textrank_ori(text, n_keywords=20):
+    words = list(jieba.cut(text))
+    candidates = extract_candidate_words(text)
+    # build graph, each node is a unique candidate
+    graph = networkx.Graph()
+    graph.add_nodes_from(set(candidates))
+    # iterate over word-pairs, add unweighted edges into graph
+    def pairwise(iterable):
+        """s -> (s0,s1), (s1,s2), (s2, s3), ..."""
+        a, b = tee(iterable)
+        next(b, None)
+        return izip(a, b)
+    for w1, w2 in pairwise(candidates):
+        if w2:
+            graph.add_edge(*sorted([w1, w2]))
+    # score nodes using default pagerank algorithm, sort by score, keep top n_keywords
+    ranks = networkx.pagerank(graph)
+    word_ranks = {word_rank[0]: word_rank[1]
+                  for word_rank in sorted(ranks.iteritems(), key=lambda x: x[1], reverse=True)[:n_keywords]}
+    keywords = set(word_ranks.keys())
+    print "keywords",keywords
+    # merge keywords into keyphrases
+    keyphrases = {}
+    j = 0
+    for i, word in enumerate(words):
+        if i < j:
+            continue
+        if word in keywords:
+            kp_words = list(takewhile(lambda x: x in keywords, words[i:i+10]))
+            print "kp_words",kp_words
+            avg_pagerank = sum(word_ranks[w] for w in kp_words) / float(len(kp_words))
+            keyphrases[' '.join(kp_words)] = avg_pagerank
+            # counter as hackish way to ensure merged keyphrases are non-overlapping
+            j = i + len(kp_words)
+    
+    return sorted(keyphrases.iteritems(), key=lambda x: x[1], reverse=True)
+
+def score_keyphrases_by_tfidf(texts, candidates='chunks'):
+
+    
+    # extract candidates from each text in texts, either chunks or words
+    if candidates == 'chunks':
+        boc_texts = [extract_candidate_chunks(text) for text in texts]
+    elif candidates == 'words':
+        boc_texts = [extract_candidate_words(text) for text in texts]
+    # make gensim dictionary and corpus
+    dictionary = gensim.corpora.Dictionary(boc_texts)
+    corpus = [dictionary.doc2bow(boc_text) for boc_text in boc_texts]
+    # transform corpus with tf*idf model
+    tfidf = gensim.models.TfidfModel(corpus)
+    corpus_tfidf = tfidf[corpus]
+    
+    return corpus_tfidf, dictionary
+
+# def extract_candidate_words(text, good_tags=set(['JJ','JJR','JJS','NN','NNP','NNS','NNPS'])):
+def extract_candidate_words(text, good_tags=set(['n','ns','nz','nr','nrt','nt','x'])):
+
+    candidates = list()
+    words = pseg.cut(text)
+    for word,flag in words:
+        if flag in good_tags:
+            if word not in stopwordList:
+                candidates.append(word)
+    return candidates
+
+def extract_candidate_chunks(text, grammar=r'KT: {(<JJ>* <NN.*>+ <IN>)? <JJ>* <NN.*>+}'):
+    import itertools, nltk, string
+    
+    # exclude candidates that are stop words or entirely punctuation
+    punct = set(string.punctuation)
+    stop_words = set(nltk.corpus.stopwords.words('english'))
+    # tokenize, POS-tag, and chunk using regular expressions
+    chunker = nltk.chunk.regexp.RegexpParser(grammar)
+    tagged_sents = nltk.pos_tag_sents(nltk.word_tokenize(sent) for sent in nltk.sent_tokenize(text))
+    all_chunks = list(itertools.chain.from_iterable(nltk.chunk.tree2conlltags(chunker.parse(tagged_sent))
+                                                    for tagged_sent in tagged_sents))
+    # join constituent chunk words into a single chunked phrase
+    candidates = [' '.join(word for word, pos, chunk in group).lower()
+                  for key, group in itertools.groupby(all_chunks, lambda (word,pos,chunk): chunk != 'O') if key]
+
+    return [cand for cand in candidates
+            if cand not in stop_words and not all(char in punct for char in cand)]
